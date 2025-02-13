c4f15a6e1e0e747f693ffab2d788eea8269be1b0
Patrick Pircher <patricklx@users.noreply.github.com>
2025-02-13 20:43:04 +0100

    support enterprise github

---
+++ src/github-api.spec.ts
@@ -0,0 +1,44 @@
+import GithubAPI from "./github-api";
+
+describe("github api", function () {
+  it("should return correct api endpoint with env var set", function () {
+    process.env.GITHUB_AUTH = "test";
+    process.env.GITHUB_DOMAIN = "github.host.com";
+    // this is also set by GitHub actions
+    delete process.env.GITHUB_API_URL;
+    let github = new GithubAPI({
+      repo: "foo",
+      rootPath: ".",
+    });
+    let fetchedUrl = "";
+    github["_fetch"] = async function (url: string) {
+      fetchedUrl = url;
+    };
+    expect(github.getBaseIssueUrl("foo")).toEqual(`https://github.host.com/foo/issues/`);
+
+    github.getUserData("foo");
+    expect(fetchedUrl).toEqual(`https://api.github.host.com/users/foo`);
+
+    github.getIssueData("foo", "2");
+    expect(fetchedUrl).toEqual(`https://api.github.host.com/repos/foo/issues/2`);
+
+    delete process.env.GITHUB_DOMAIN;
+
+    process.env.GITHUB_API_URL = "https://api.github.host2.com";
+
+    github = new GithubAPI({
+      repo: "foo",
+      rootPath: ".",
+    });
+    github["_fetch"] = async function (url: string) {
+      fetchedUrl = url;
+    };
+    expect(github.getBaseIssueUrl("foo")).toEqual(`https://github.com/foo/issues/`);
+
+    github.getUserData("foo");
+    expect(fetchedUrl).toEqual(`https://api.github.host2.com/users/foo`);
+
+    github.getIssueData("foo", "2");
+    expect(fetchedUrl).toEqual(`https://api.github.host2.com/repos/foo/issues/2`);
+  });
+});
